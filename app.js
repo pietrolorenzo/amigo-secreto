791cae675673c8b28453e14dfe4df6607478e451
pietrolorenzo <pietro_lorenzo@outlook.com>
2025-08-16 21:23:53 -0300

    Add files via upload
    Finalizando projeto

--- app.js
+++ app.js
@@ -2,16 +2,8 @@ console.log("Começou");
 
 var names = [];
 
-
 document.getElementById('buttonAdd').addEventListener('click', verifyName);
 document.getElementById('buttonAdd').addEventListener('click', updateList);
-
-document.getElementById('buttonRemove').addEventListener('click', remove);
-document.getElementById('buttonRemove').addEventListener('click', updateList);
-
-document.getElementById('buttonDraw').addEventListener('click', draw);
-document.getElementById('buttonRemove').addEventListener('click', updateList);
-
 
 function verifyName() {
     let name = document.getElementById("name").value.trim(); 
@@ -25,49 +17,19 @@ function verifyName() {
     }
 }
 
-function remove() {
-    let name = document.getElementById("name").value.trim();
-
-    if (name.length > 0) {
-        let index = names.indexOf(name); // procura no array
-
-        if (index !== -1) {
-            names.splice(index, 1); // remove pelo índice
-            limparCampo();
-            updateList();
-            console.log(names);
-        } else {
-            alert(`O nome ${name} não foi encontrado na lista.`); // crases
-        }
-    } else {
-        alert("Por favor, insira um nome.");
-    }
-}
-
-
 function limparCampo() {
     document.getElementById('name').value = '';
 }
 
 function updateList() {
-    var ol = document.getElementById("nameList");
-    ol.innerHTML = "";
+    var ul = document.getElementById("nameList");
+    ul.innerHTML = "";
 
     for (var i = 0; i < names.length; i++) {
         var li = document.createElement("li");
         li.textContent = names[i];
-        ol.appendChild(li);
+        ul.appendChild(li);
     }
-}
-
-function draw(){
-    if (names != ""){
-    let chosenName = names[Math.floor(Math.random() * names.length)];
-    console.log(chosenName);
-
-}else{
-    alert("Digite pelo menos dois nomes.");
-}
 }
 
 updateList();   
